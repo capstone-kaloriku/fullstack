2863bf01bc72fcf7d4d479d8534a2273d6b7942a
kevinalvarel <alvarelkevin12@gmail.com>
2026-04-15 10:14:47 +0700

    feat: initial commit

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1>Kaloriku</h1>
+      <div>Selamat datang dikaloriku</div>
+    </div>
+  );
+}
